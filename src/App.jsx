357ffcbd08507e96e437c263306083eb20b56b52
psattri87 <psattri87@gmail.com>
2025-01-24 20:29:35 +0530

    fixing error-4

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,14 @@ function App() {
     return isValid;
   }
 
+  function validateEmail(email) {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    const isValid = regex.test(email);
+    if (!isValid) {
+      alert("Invalid email. Please check your email address.");
+    }
+  }
+
   function validateDate(dob) {
     if (new Date(dob).getTime() > new Date().getTime()) {
       alert("Invalid phone number. Please enter a 10-digit phone number.");
@@ -44,10 +52,12 @@ function App() {
 
   const handleSubmit = () => {
     validatePhoneNumber(formData.phone);
+    validateEmail(formData.email);
     validateDate(formData.dob);
     console.log("Form submitted:", formData);
     setFormData({ ...formData, name: "", email: "", phone: "", dob: "" });
   };
+
   const openForm = () => {
     setFormVisible(true);
   };
